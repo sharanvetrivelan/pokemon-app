764aa608a79e4c8c0c0d76fc46ca82dc3eb6d70c
sharanvetrivelan <164971784+sharanvetrivelan@users.noreply.github.com>
2025-01-16 21:14:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,13 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 
-dotenv.config(); // Load environment variables
+dotenv.config(); 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: ['http://localhost:3000','http://localhost:5173'],//Replace your frontend webpage address when you run both locally and online
+  origin: ['http://localhost:3000','http://localhost:5173'],//This is my LocalHost Frontend Web Address
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   credentials: true
 }));
@@ -27,11 +27,11 @@ const { Pool } = require('pg');
 
 // Database connection
 const pool = new Pool({
-  user: 'postgres', // Replace with your DB username
+  user: 'postgres', // My DB username
   host: 'localhost',
-  database: 'pokemon',
-  password: 'pokemon', // Replace with your DB password
-  port: 5432, // Default PostgreSQL port
+  database: 'pokemon', // My DB name
+  password: 'pokemon', // My DB password
+  port: 5432, 
 });
 
 pool.connect((err) => {
@@ -43,8 +43,8 @@ pool.connect((err) => {
 });
 app.get('/api/pokemon', async (req, res) => {
     try {
-      const limit = parseInt(req.query.limit) || 10; // Default limit to 10
-      const offset = parseInt(req.query.offset) || 0; // Default offset to 0
+      const limit = parseInt(req.query.limit) || 10; 
+      const offset = parseInt(req.query.offset) || 0; 
       const result = await pool.query('SELECT * FROM pokemon ORDER BY id');
       const paginatedPokemon = result.rows.slice(offset, offset + limit);
       res.json(paginatedPokemon);
